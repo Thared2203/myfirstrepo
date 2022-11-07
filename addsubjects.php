41236ba2b0b86eb62ad402de0eca48e7826f753e
Thared2203 <tilley.tae@oundleschool.org.uk>
2022-11-07 12:45:34 +0000

    YAY

--- addsubjects.php
+++ addsubjects.php
@@ -1,4 +1,6 @@
 <?php
+array_map("htmlspecialchars", $_POST);
+
 header('Location:subjects.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
--- subjects.php
+++ subjects.php
@@ -10,6 +10,8 @@
   <input type="submit" value="Add Subject">
 </form>
 <?php
+    array_map("htmlspecialchars", $_POST)
+    
 	include_once('connection.php');
 	$stmt = $conn->prepare("SELECT * FROM TblSubjects");
 	$stmt->execute();
